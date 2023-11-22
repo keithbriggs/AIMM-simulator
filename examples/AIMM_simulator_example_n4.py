e280127d052458bb5d813c331b7f6dad8241152a
george <george.davy@bt.com>
2023-11-22 16:38:44 +0000

    updated path for test files and merged rsrp and CQI reports for the UEs

--- examples/AIMM_simulator_example_n4.py
+++ examples/AIMM_simulator_example_n4.py
@@ -1,6 +1,6 @@
 # Keith Briggs 2022-11-14
 from sys import path
-path = "/home/davygx/Documents/haps_dev_ge/AIMM-simulator/src/AIMM_simulator" + path
+path = ["/home/davygx/Documents/haps_dev_ge/AIMM-simulator/src/AIMM_simulator"] + path
 from AIMM_simulator import Sim,Logger,MME,Scenario,RIC,np_array_to_str
 from numpy.random import standard_normal,seed
 
